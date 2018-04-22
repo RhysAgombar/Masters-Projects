4d9036fcb5fb9a3848825eff82e2c4f6ced1376e
Rhys Agombar <RhysAgombar@users.noreply.github.com>
2018-04-22 02:00:43 +0200

    Randomizing added

--- Artificial Life/Exercise 2/Exercise 2/Exercise2.cpp
+++ Artificial Life/Exercise 2/Exercise 2/Exercise2.cpp
@@ -10,22 +10,27 @@ const int cellSize = 83;
 const int seedCell = 42;
 int cCells[cellSize], pCells[cellSize]; // Current cells and previous cells
 
-int init(char condition) {
+int init(char condition, int r) {
 
   for (int i = 0; i < cellSize; i++) {
     cCells[i] = 0;
     pCells[i] = 0;
   }
 
+  if (r != 0 && r != 1) {
+    printf("Invalid value for r.\n");
+    return 0;
+  }
+
   if (condition == 'S' or condition == 's') {
-    printf("Seed\n");
+    printf("Seed, r=%d\n", r);
 
     pCells[seedCell] = 1;
   }
   else if (condition == 'R' or condition == 'r') {
-    printf("Random\n");
+    printf("Random, r=%d\n", r);
 
-    for (int i = 0; i < cellSize + 1; i++) {
+    for (int i = 2; i < cellSize - 1; i++) { // Skips the first and last two cells to create a boundary.
       int chance = rand() % 10;
       if (chance < 5) {
         pCells[i] = 0;
@@ -51,14 +56,23 @@ int init(char condition) {
 
 }
 
+void step(int r) {
+
+}
+
 int main()
 {
   srand(time(NULL));
   char cond = 'r';
+  int r = 1;
 
-  int res = init(cond);
+  int res = init(cond, r);
   if (res == 0) {
     return 0;
+  }
+
+  for (int i = 0; i < 20; i++) {
+    step(r);
   }
 
   return 0;
