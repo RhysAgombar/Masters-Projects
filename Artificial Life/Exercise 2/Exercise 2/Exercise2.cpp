e0ab5efcf0fa34e3a8f61c73cdf8f0f576d79e95
Rhys Agombar <RhysAgombar@users.noreply.github.com>
2018-04-22 18:10:10 +0200

    Added Comments

--- Artificial Life/Exercise 2/Exercise 2/Exercise2.cpp
+++ Artificial Life/Exercise 2/Exercise 2/Exercise2.cpp
@@ -1,5 +1,4 @@
-﻿// Exercise2.cpp : Defines the entry point for the console application.
-//
+﻿// Exercise 2 Programming Assignment - By: Rhys Agombar & Joanna Polewczyk
 
 #include <stdio.h>
 #include <tchar.h>
@@ -11,93 +10,104 @@
 
 using namespace std;
 
-const int cellSize = 83;
-const int seedCell = 42;
-const int startPos = 2;
-const int endPos = 80;
+const int cellSize = 83; // Size of Cell Array
+const int seedCell = 42; // Seed Cell Index
+const int startPos = 2; // Start Position Index
+const int endPos = 80; // End Position Index
 int cCells[cellSize], pCells[cellSize]; // Current cells and previous cells
-int rsLen = 0;
-string *ruleSet;
-string ruleString = "";
+int ssLen = 0; // Length of the state set (this will be properly set in the initStateSet function
+string *stateSet; // Uninitialized array of states
+string ruleString = ""; // The binary representation of the CA Rule Number
 
+/// <summary>
+/// Converts the CA number to a binary string which will be used in the evalStates function to return the correct new state of a cell.
+/// </summary>
+/// <param name="rule">The rule number to be converted</param>
+/// <param name="r">The radius of the CA</param>
+/// <returns></returns>
 string ruleToString(int rule, int r) {
 
-  int bitnum = pow(2, (2 * r + 1));
+  int bitnum = pow(2, (2 * r + 1)); //Calculates the number of bits the string will contain
   string out = "";
 
   int ruleHolder = rule;
-for (int i = bitnum - 1; i >= 0; i--) {
-  int val = pow(2, i);
-  if (ruleHolder - val >= 0) {
-    ruleHolder -= val;
-    out.append("1");
+  for (int i = bitnum - 1; i >= 0; i--) { // Counting down from the highest bit to the lowest bit...
+    int val = pow(2, i); // Find the value of that bit (2^7, 2^6, 2^5, etc...)
+    if (ruleHolder - val >= 0) { // If the value fits into the rule number (150 - 128 = 22, etc.)...
+      ruleHolder -= val;
+      out.append("1"); // Update the value and append a 1 to the binary representation
+    }
+    else {
+      out.append("0"); // If not, append a 0 to the representation as the current bit does not fit.
+    }
   }
-  else {
-    out.append("0");
-  }
-}
 
 return out;
 }
 
-void initRuleSet(int r) {
-  int bitnum = 2 * r + 1;
-  int bitlen = pow(2, bitnum);
-  rsLen = bitlen;
-  ruleSet = new string[rsLen];
+/// <summary>
+/// Initializes the state set. Each state is stored as a set of binary bits, representing the states of the previous cells (11111, 101, 100, 001, etc...)
+/// </summary>
+/// <param name="r">The radius of the CA</param>
+/// <returns></returns>
+void initStateSet(int r) {
+  int bitnum = 2 * r + 1; // Number of bits in each state. For r=1, there are a total of 3 bits in each state. 111 = 0, 110 = 1, etc.
+  int bitlen = pow(2, bitnum); // Length of the stateset
+  ssLen = bitlen;
+  stateSet = new string[ssLen]; // Creating an array of strings
 
-  for (int j = 1; j <= bitlen; j++) {
+  for (int j = 1; j <= bitlen; j++) { // The same process as in ruleToString
     int holder = bitlen - j;
-    string curRule = "";
+    string curState = "";
     for (int i = bitnum - 1; i >= 0; i--) {
       int val = pow(2, i);
       if (holder - val >= 0) {
         holder -= val;
-        curRule.append("1");
+        curState.append("1");
       }
       else {
-        curRule.append("0");
+        curState.append("0");
       }
     }
 
-    ruleSet[j - 1] = curRule;
+    stateSet[j - 1] = curState; // Save the state
   }
-
-  for (int i = 0; i < rsLen; i++) {
-    printf(ruleSet[i].c_str());
-    printf("\n");
-  }
-
   return;
-
 }
 
+/// <summary>
+/// Initializes the program. 
+/// </summary>
+/// <param name="rule">The CA Number</param>
+/// <param name="r">The radius of the CA</param>
+/// <param name="condition">The starting condition</param>
+/// <returns></returns>
 int init(int rule, int r, char condition) {
 
-  ruleString = ruleToString(rule, r);
-  initRuleSet(r);
+  ruleString = ruleToString(rule, r); // Sets the ruleString
+  initStateSet(r); // Initializes the state set.
 
-  for (int i = 0; i < cellSize; i++) {
+  for (int i = 0; i < cellSize; i++) { // Initialize arrays to 0
     cCells[i] = 0;
     pCells[i] = 0;
   }
 
-  if (r != 1 && r != 2) {
+  if (r != 1 && r != 2) { // Check for invalid radius
     printf("Invalid value for r.\n");
     return 0;
   }
 
-  if (condition == 'S' or condition == 's') {
-    printf("Seed, r=%d\n", r);
+  if (condition == 'S' or condition == 's') { // If seed starting condition, only set seed cell to be 1
+    printf("Seed, r=%d, rule=%d\n", r, rule);
 
     pCells[seedCell] = 1;
   }
   else if (condition == 'R' or condition == 'r') {
-    printf("Random, r=%d\n", r);
+    printf("Random, r=%d, rule=%d\n", r, rule);
 
     for (int i = startPos; i <= endPos; i++) { // Skips the first and last two cells to create a boundary.
       int chance = rand() % 10;
-      if (chance < 5) {
+      if (chance < 5) { // 50% chance of the cell being a 0 or a 1
         pCells[i] = 0;
       }
       else {
@@ -111,7 +121,7 @@ int init(int rule, int r, char condition) {
     return 0;
   }
 
-  for (int i = 0; i < cellSize; i++) {
+  for (int i = 0; i < cellSize; i++) { // print the initial starting condition of the cells
     printf("%d ", pCells[i]);
   }
 
@@ -120,33 +130,47 @@ int init(int rule, int r, char condition) {
   return 1;
 }
 
-int evalRules(int pos, int r) {
+/// <summary>
+/// Determines the state that a cell should be in based on the rule set and previous cell states
+/// </summary>
+/// <param name="pos">The position of the cell to be evaluated</param>
+/// <param name="r">The radius of the CA</param>
+/// <returns></returns>
+int evalStates(int pos, int r) {
   string state = "";
-  for (int i = pos - r; i <= pos + r; i++) {
-    state.append(to_string(pCells[i]));
+  for (int i = pos - r; i <= pos + r; i++) { // check the states of the cell neighbors from the previous time step
+    state.append(to_string(pCells[i])); // construct a binary representation of the cell states eg. 110, 101, 11001, etc.
   }
 
-  for (int i = 0; i < rsLen; i++){
-    if (ruleSet[i] == state) {
-      return (int)ruleString.at(i) - '0';
+  for (int i = 0; i < ssLen; i++){ // for every state in the stateset
+    if (stateSet[i] == state) { // check if the state is relevant (the same as the cell state constructed in the above step)
+      return (int)ruleString.at(i) - '0'; // return the result state as given by the binary representation of the CA number.
+      // ruleString will be a string of binary, eg. 11001011. Each bit corresponds to the result of a set of previous states, therefore by returning the 
+      // bit at the index of the previous state found, we get the result
     }
   }
 
-  return 1;
+  return -1; // If this gets hit, something has gone horribly wrong.
 }
 
+/// <summary>
+/// Proceeds to the next timestep and generates the cell states based on the evaluation of the states.
+/// </summary>
+/// <param name="r">The radius of the CA</param>
+/// <returns></returns>
 void step(int r) {
-  for (int i = startPos; i <= endPos; i++) {
-    cCells[i] = evalRules(i, r);
+  for (int i = startPos; i <= endPos; i++) { // For each cell in the current cell list
+    cCells[i] = evalStates(i, r); // Set it's state equal to the result of the rule evaluation
   }
 
-  for (int i = 0; i < cellSize; i++) {
-    printf("%d ", cCells[i]);
-    pCells[i] = cCells[i];
-    cCells[i] = 0;
+  for (int i = 0; i < cellSize; i++) { // for every cell...
+    printf("%d ", cCells[i]); // Print the current cells to the console
+    pCells[i] = cCells[i]; // Save the current values of cells to the previous cells list
+    cCells[i] = 0; // set all current cells to state 0 in preparation for the next step.
   }
   printf("\n");
 }
+
 
 int main(int argc, char* argv[])
 {
@@ -155,13 +179,13 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  srand(time(NULL));
+  srand(time(NULL)); // Seeds the randomizer function with a value from the system clock
 
   int r = 0;
   int rule = 0;
   char cond = 'r';
 
-  try {
+  try { // Accept and format the input arguments
     string rS = argv[1];
     string ruleS = argv[2];
     string condS = argv[3];
@@ -170,7 +194,7 @@ int main(int argc, char* argv[])
     rule = stoi(ruleS);
     cond = condS[0];
   }
-  catch (exception e) {
+  catch (exception e) { // If anything fails, something is wrong with the formatting. Display message to user.
     printf("Invalid inputs. Please format the arguments as follows:\n");
     printf("Radius - Integer (1 or 2)\n");
     printf("Cellular Automata Rule - Integer (eg. 150)\n");
@@ -179,13 +203,14 @@ int main(int argc, char* argv[])
   }
 
   
-  int res = init(rule, r, cond);
-  if (res == 0) {
+  int res = init(rule, r, cond); // Initialize the system
+
+  if (res == 0) { // If an error code (0) is returned, quit the program.
     return 0;
   }
 
-  for (int i = 0; i < 20; i++) {
-    step(r);
+  for (int i = 0; i < 20; i++) { 
+    step(r); // This is the function that generates the next timestep
   }
 
   return 0;
